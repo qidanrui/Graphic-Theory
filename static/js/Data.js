110bc0242d2075e957dc046120704532793fdce2
Jerry Tang <thss15_tangrj@163.com>
2017-01-11 00:35:45 +0800

    Update Data.js
    In fact this file may have no use anymore

--- static/js/Data.js
+++ static/js/Data.js
@@ -1,14 +1,14 @@
-var edge = [{"node1": 0, "node2": 1, "weight": 7},
-			{"node1": 0, "node2": 3, "weight": 5},
-			{"node1": 1, "node2": 2, "weight": 8},
-			{"node1": 1, "node2": 3, "weight": 9},
-			{"node1": 1, "node2": 4, "weight": 7},
-			{"node1": 2, "node2": 4, "weight": 5},
-			{"node1": 3, "node2": 4, "weight": 15},
-			{"node1": 3, "node2": 5, "weight": 6},
-			{"node1": 4, "node2": 5, "weight": 8},
-			{"node1": 4, "node2": 6, "weight": 9},
-			{"node1": 5, "node2": 6, "weight": 11}];
+var links = [{"source": 0, "target": 1, "value": 7},
+			{"source": 0, "target": 3, "value": 5},
+			{"source": 1, "target": 2, "value": 8},
+			{"source": 1, "target": 3, "value": 9},
+			{"source": 1, "target": 4, "value": 7},
+			{"source": 2, "target": 4, "value": 5},
+			{"source": 3, "target": 4, "value": 15},
+			{"source": 3, "target": 5, "value": 6},
+			{"source": 4, "target": 5, "value": 8},
+			{"source": 4, "target": 6, "value": 9},
+			{"source": 5, "target": 6, "value": 11}];
 
 var vertex = [{"ID": 0},
 			  {"ID": 1},
@@ -21,20 +21,20 @@ var vertex = [{"ID": 0},
 var undirectedGraph = {};
 var directedGraph = {};
 var numOfVertex = vertex.length;
-var numOfEdge = edge.length;
+var numOfLinks = links.length;
 
 for (var i = 0; i < numOfVertex; ++i) {
 	undirectedGraph[vertex[i].ID] = [];
 	directedGraph[vertex[i].ID] = [];
 }
 
-for (var i = 0; i < numOfEdge; ++i) {
-	undirectedGraph[edge[i].node1].push({"ID": edge[i].node1, "next": edge[i].node2, "weight": edge[i].weight});
-	undirectedGraph[edge[i].node2].push({"ID": edge[i].node2, "next": edge[i].node1, "weight": edge[i].weight});
+for (var i = 0; i < numOfLinks; ++i) {
+	undirectedGraph[links[i].source].push({"ID": links[i].source, "next": links[i].target, "value": links[i].value});
+	undirectedGraph[links[i].target].push({"ID": links[i].target, "next": links[i].source, "value": links[i].value});
 }
 
-for (var i = 0; i < numOfEdge; ++i) {
-	directedGraph[edge[i].node1].push({"ID": edge[i].node1, "next": edge[i].node2, "weight": edge[i].weight});
+for (var i = 0; i < numOfLinks; ++i) {
+	directedGraph[links[i].source].push({"ID": links[i].source, "next": links[i].target, "value": links[i].value});
 }
 
 function ThresholdList(t)
@@ -44,9 +44,9 @@ function ThresholdList(t)
 		list[vertex[i].ID] = [];
 	}
 
-	for (var i = 0; i < numOfEdge; ++i) {
-		if (edge[i].weight > t) {
-			list[edge[i].node1].push({"ID": edge[i].node1, "next": edge[i].node2, "weight": edge[i].weight});
+	for (var i = 0; i < numOfLinks; ++i) {
+		if (links[i].value > t) {
+			list[links[i].source].push({"ID": links[i].source, "next": links[i].target, "value": links[i].value, "IDOfLink": i});
 		}
 	}
 	return list;
